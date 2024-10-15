c71820819d6d94f36b84f43b94c5b8d122497484
Rafaartdev <silveira.orafa@gmail.com>
2024-10-15 16:28:38 -0300

    utilizando arquivos

---
+++ 01-heloworld.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Experiências com PHP</h1>
+    <hr>
+
+    <!-- denilitador de abertura -->
+<?php
+echo "Olá!";
+echo "<p>Ptogradador Web - <p>";
+echo "<p style= 'color:red'>Senac Penha</p>";
+
+?>
+    
+</body>
+</html>
