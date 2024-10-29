b0bd534ab797c4af0e06bc7ecb2d155cb64ca903
Rafaartdev <silveira.orafa@gmail.com>
2024-10-29 16:35:17 -0300

    lista de meses
    editado coloração

--- exercicio-02.php
+++ exercicio-02.php
@@ -1,23 +1,40 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>meses do ano</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    
+    <style>
+        li:nth-child(odd) {
+            color: blue;
+        }
+
+        li:nth-child(evn) {
+            color: brown;
+        }
+    </style>
 </head>
+
 <body>
-     <h1>Lista de Meses</h1>
-     <?php 
-     $meses = ["janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto","Setembro","Outubro", "Novembro", "Desembro"];
-?>
-<ol><ol
+    <h1>Lista de Meses</h1>
 
+    <ol>
+        <?php
+        $meses = ["janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Desembro"];
+        ?>
+        <ol>
 
-></ol>
+            <?php
+            foreach ($meses as $mes) {
+            ?>
 
+                <li><?= $mes ?></li>
 
-    
+            <?php } ?>
+
+        </ol>
 </body>
+
 </html>
